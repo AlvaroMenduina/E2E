692c538c3e2ed7fcf52b8259ad32262d2076f75d
AlvaroMenduina <alvaro.menduina@gmail.com>
2020-07-25 14:59:03 +0100

    feat: Added Config # in object coordinates plot

--- e2e_raytrace_fast.py
+++ e2e_raytrace_fast.py
@@ -46,10 +46,12 @@ def detector_plots(zosapi, sys_mode, ao_modes, spaxel_scale, spaxels_per_slice, 
         focal_coord.append(foc_xy)
         object_coord.append(obj_xy)
 
+        # (1) Detector Focal coordinates plot
+
         fig, ax = plt.subplots(1, 1, figsize=(15, 10))
 
         N_configs = foc_xy.shape[0]
-        colors = cm.Reds(np.linspace(0.5, 0.75, N_waves))
+        colors = cm.Reds(np.linspace(0.5, 1.0, N_waves))
 
         # we need to know which Field point is the inner edge of the slit
         # i.e. obj_x ~ 0.0, this varies between odd and even config
@@ -65,8 +67,8 @@ def detector_plots(zosapi, sys_mode, ao_modes, spaxel_scale, spaxels_per_slice, 
             ax.scatter(x, y, color=colors[j_wave], s=10)
 
             # add the lambda
-            x_max, y_mean = np.max(x), np.mean(y)
-            ax.text(x=x_max + 1, y=y_mean, s=r'$\lambda_{%d}$' % (wavelength_idx[j_wave]))
+            x_max, y_mean = np.max(x), np.min(y)
+            ax.text(x=x_max + 2, y=y_mean, s=r'$\lambda_{%d}$' % (wavelength_idx[j_wave]))
 
             for k_config in range(N_configs):
 
@@ -77,7 +79,10 @@ def detector_plots(zosapi, sys_mode, ao_modes, spaxel_scale, spaxels_per_slice, 
 
                 # Add a label with the configuration number
                 x_c, y_c = foc_xy[k_config, j_wave, 1, 0], foc_xy[k_config, j_wave, 1, 1]
-                ax.text(x=x_c, y=y_c + 0.5, s='%d' % (k_config + 1))
+                if k_config >= N_configs//2:
+                    ax.text(x=x_c, y=y_c - 1.5, s='%d' % (k_config + 1))
+                else:
+                    ax.text(x=x_c, y=y_c + 0.5, s='%d' % (k_config + 1))
 
         ax.set_xlabel('Detector X [mm]')
         ax.set_ylabel('Detector Y [mm]')
@@ -85,6 +90,47 @@ def detector_plots(zosapi, sys_mode, ao_modes, spaxel_scale, spaxels_per_slice, 
         ax.set_title(title)
 
         fig_name = "CONFIGS_DET_IFU-%s_%s_SPEC_%s_MODE_%s_%s" % (ifu_section, spaxel_scale, grating, sys_mode, ao_modes[0])
+
+        save_path = os.path.join(results_path, analysis_dir)
+        if os.path.isfile(os.path.join(save_path, fig_name)):
+            os.remove(os.path.join(save_path, fig_name))
+        fig.savefig(os.path.join(save_path, fig_name))
+
+        # (2) Object space coordinates
+
+        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+
+        # we need to know which Field point is the inner edge of the slit
+        # i.e. obj_x ~ 0.0, this varies between odd and even config
+        obj_x = obj_xy[:, :, 0]
+        # For each configuration we select the field point closer to f_x = 0
+        # by sorting the absolute value of the fx coordinates
+        edge_field = [np.argsort(np.abs(obj_x[i]))[0] for i in range(N_configs)]
+
+        # We plot all 3 field points across all configs for a given wavelength
+        x, y = obj_xy[:, :, 0].flatten(), obj_xy[:, :, 1].flatten()
+        ax.scatter(x, y, color='red', s=10)
+
+        for k_config in range(N_configs):
+
+            # Mark the inner edge of the slit
+            i_edge = edge_field[k_config]
+            x_edge, y_edge = obj_xy[k_config, i_edge, 0], obj_xy[k_config, i_edge, 1]
+            ax.scatter(x_edge, y_edge, color='black', s=15, facecolors='none')
+
+            # Add a label with the configuration number
+            x_c, y_c = obj_xy[k_config, 1, 0], obj_xy[k_config, 1, 1]
+            if k_config >= N_configs//2:
+                ax.text(x=x_c + 0.02, y=y_c - 0.01, s='%d' % (k_config + 1), fontsize='small')
+            else:
+                ax.text(x=x_c + 0.02, y=y_c - 0.01, s='%d' % (k_config + 1), fontsize='small')
+
+        ax.set_xlabel('Object X [mm]')
+        ax.set_ylabel('Object Y [mm]')
+        title = r'IFU-%s | %s | %s | %s %s' % (ifu_section, spaxel_scale, grating, sys_mode, ao_modes[0])
+        ax.set_title(title)
+
+        fig_name = "CONFIGS_OBJ_IFU-%s_%s_SPEC_%s_MODE_%s_%s" % (ifu_section, spaxel_scale, grating, sys_mode, ao_modes[0])
 
         save_path = os.path.join(results_path, analysis_dir)
         if os.path.isfile(os.path.join(save_path, fig_name)):
@@ -105,7 +151,7 @@ if __name__ == """__main__""":
     # [*] This is the bit we have to change when you run the analysis in your system [*]
     sys_mode = 'HARMONI'
     ao_modes = ['NOAO']
-    spaxel_scale = '60x30'
+    spaxel_scale = '20x20'
     spaxels_per_slice = 3       # How many field points per Slice to use
     # gratings = ['VIS', 'Z_HIGH', 'IZ', 'J', 'IZJ', 'H', 'H_HIGH', 'HK', 'K', 'K_LONG', 'K_SHORT']
     gratings = ['H']
